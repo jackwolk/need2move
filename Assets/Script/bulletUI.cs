3c35f1ad906516d24234ece34fbb77caef3d9872
Aidan Ranson <aidranson@gmail.com>
2023-10-21 19:01:28 -0400

    Bullet UI
    Added Bullet UI Functionality

--- Assets/Script/bulletUI.cs
+++ Assets/Script/bulletUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -9,16 +10,104 @@ public class bulletUI : MonoBehaviour
 {
 
     public GunGun gunscr;
+    Vector3 StartPos;
+    RawImage [] bullet_array;
+    int tempMax;
+    public RawImage bulletImage;
+    public GameObject GunGun;
+    public Canvas canvas;
+
+    public bool hasGun = true;
+    public bool doOnce = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        tempMax = gunscr.maxBullet;
         gunscr = GameObject.Find("Gun Gun").GetComponent<GunGun>();
+        bullet_array = new RawImage[tempMax];
+
+        StartPos = new Vector3(-862, -424, 0);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hasGun == true && doOnce == false)
+        {
+            for (int i = 0; i < bullet_array.Length; i++)
+            {
+                RawImage image = Instantiate(bulletImage, canvas.transform, true);
+                image.rectTransform.anchoredPosition = StartPos + new Vector3(i * 100, 0, 0);
+                image.name = "BulletUIImage #" + i;
+            }
+            doOnce = true;
+        }
+
+
+
+
+        if (tempMax != gunscr.maxBullet)
+        {
+            tempMax = gunscr.maxBullet;
+            bullet_array = new RawImage[tempMax];
+        }
+
+        if (GunGun.activeInHierarchy == true)
+        {
+
+            if (Input.GetMouseButtonDown(0) && gunscr.currentBullet > 0)
+            {
+                int current = gunscr.currentBullet - 1;
+                Debug.Log(current);
+                Destroy(GameObject.Find("BulletUIImage #" + current));
+            }
+
+            if (Input.GetKeyDown(gunscr.reloadKey))
+            {
+                for (int i = 0; i < bullet_array.Length; i++)
+                {
+                    Destroy(GameObject.Find("BulletUIImage #" + i));
+                }
+
+                for (int i = 0; i < bullet_array.Length; i++)
+                {
+                    RawImage image = Instantiate(bulletImage, canvas.transform, true);
+                    image.rectTransform.anchoredPosition = StartPos + new Vector3(i * 100, 0, 0);
+                    image.name = "BulletUIImage #" + i;
+                }
+
+            }
+
+
+        }
+
+
+
         
+        
+
+
+    }
+
+
+    public void ForceReload()
+    {
+        for (int i = 0; i < bullet_array.Length; i++)
+        {
+            Destroy(GameObject.Find("BulletUIImage #" + i));
+        }
+
+        for (int i = 0; i < bullet_array.Length; i++)
+        {
+            RawImage image = Instantiate(bulletImage, canvas.transform, true);
+            image.rectTransform.anchoredPosition = StartPos + new Vector3(i * 100, 0, 0);
+            image.name = "BulletUIImage #" + i;
+        }
+
+        gunscr.currentBullet = gunscr.maxBullet;
     }
 }
